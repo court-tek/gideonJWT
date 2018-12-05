d83abb7e5c0598e610ff83f576507bc532d49c2a
court-tek <dishoughng@gmail.com>
2018-12-05 12:44:41 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// Server Setuo
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+server.listen(PORT);
+console.log(`Server listening on: ${PORT}`);
